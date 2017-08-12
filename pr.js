0e60698ba864703902844427861ad00eab455390
av-mido <av-mido@users.noreply.github.com>
2017-08-12 16:55:27 +0400

    working version, need to clean this up

--- pr.js
+++ pr.js
@@ -1,36 +1,64 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: "debug"
+});
 var fs = require('fs');
 
 var credentials = JSON.parse(fs.read('credentials.json'));
+console.log("Credentials are: ", JSON.stringify(credentials));
 
 function selectForm()
 {
     var targetDocument = document.querySelector('frame[name="bottomFrame"]').contentDocument.querySelector('frame[name="mainFrame"]').contentDocument;
     var e = targetDocument.querySelector('.txtFill_singleLine');
-    //console.log(e.getAttribute("name"));
-    e.selectedIndex = 7;
+    console.log("this is a log message inside selectForm.");
+    // this.log("this is a log message inside selectForm.");
+
+    e.selectedIndex = 8; // was 7!
     e.onchange();
+    // var evt = document.createEvent("UIEvents"); // or "HTMLEvents"
+    // evt.initUIEvent("change", true, true);
+    // e.dispatchEvent(evt);
 }
 
 function fillForm(credentials)
 {
+    console.log("this is a log message inside fillForm.");
+    console.log("document: " + document);
+    console.log("first query selector: " + document.querySelector('frame[name="bottomFrame"]').contentDocument);
+    console.log("second query selector: " + document.querySelector('frame[name="bottomFrame"]').contentDocument.querySelector('frame[name="mainFrame"]').contentDocument);
     var targetDocument = document.querySelector('frame[name="bottomFrame"]').contentDocument.querySelector('frame[name="mainFrame"]').contentDocument;
-    var e = targetDocument.querySelector('input[name="apptDetails.identifier1"]');
-    //console.log(e.getAttribute("name"));
+
+    // console.log("targetDocument.body: " + targetDocument.body);
+    // console.log("targetDocument.body.innerHTML: " + targetDocument.body.innerHTML);
+
+    var e = targetDocument.querySelector('input[id="apptDetails.identifier1"]');
+    console.log("is e null? " + e);
+    console.log("credentials.fin: " + credentials.fin);
+    console.log("e.name: " + e.getAttribute("name"));
+    
+
+
     e.value = credentials.fin;
     var e = targetDocument.querySelector('input[name="apptDetails.identifier2"]');
     e.value = credentials.peopleCount;
     var e = targetDocument.querySelector('input[name="apptDetails.identifier3"]');
     e.value = credentials.contact;   
     var e = targetDocument.querySelector('input[name="Submit"]');
+    console.log("About to click at end of fillForm");
     e.click();       
 }
 
 function countAvailableDates()
 {
     var obj = {};
-    var e = document.querySelector('#main > form > table:nth-child(81) > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td.txtBlk > strong');    
+    // var targetDocument = document.querySelector('frame[name="bottomFrame"]').contentDocument.querySelector('frame[name="mainFrame"]').contentDocument;
+    var e = document.querySelector('#main > form > table:nth-child(83) > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td.txtBlk > strong');
+ // var e = document.querySelector('#main > form > table:nth-child(81) > tbody > tr:nth-child(6) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td.txtBlk > strong');    
     var txt = e.textContent;
+
+    console.log("countAvailableDates txt: ", txt);
+
     obj.month = txt.replace(/(?:\r\n|\r|\n|\t)/g,'').trim();    
     var e = document.querySelectorAll('td.cal_AS');    
     obj.available = e.length;
@@ -47,21 +75,28 @@ function countAvailableDates()
     var e = document.querySelectorAll('td.cal_PH');    
     obj.holidays = e.length; 
 
-    //console.log(JSON.stringify(obj,null,2));
+    // console.log(JSON.stringify(obj)); // ,null,2));
     
     return obj;
 }
 
-casper.on('remote.message', function(msg) {
-    this.echo('remote message caught: \n' + msg);
-})
+casper.on('remote.message', function (message) {
+  this.echo('Message: ' + message);
+});
+
+//Code to display errors from the page
+ casper.on('page.error', function (msg, trace) {
+     console.log('Error: ' + msg, 'ERROR');
+ });
 
 casper.start('https://eappointment.ica.gov.sg/ibook/index.do',function(){
     //this.capture('pr.png');
+    console.log("starting...");
     this.evaluate(selectForm);
 });
 
 casper.then(function(){
+    console.log("Calling fillForm...");
    this.evaluate(fillForm,credentials);
 });
 
@@ -73,13 +108,15 @@ for(i = 0; i<12; ++i)
     casper.withFrame('bottomFrame', function() {
         this.withFrame('mainFrame',function(){
            obj = this.evaluate(countAvailableDates);
-           //console.log(JSON.stringify(obj,null,2));
+           console.log("within for loop: ", JSON.stringify(obj,null,2));
            jsonArr.push(obj);
         });
-    });
+    });    
 
     casper.withFrame('bottomFrame', function() {
         this.withFrame('mainFrame',function(){
+               console.log("i: ", i);
+                console.log("jsonArr: ", JSON.stringify(jsonArr));
             	casper.click('a[href="javascript:doNextMth(document.forms[0]);"]');
         });
     });
@@ -90,6 +127,7 @@ for(i = 0; i<12; ++i)
 }
 
 casper.then(function(){
+    console.log("Finished. Months: ");
     console.log(JSON.stringify(jsonArr));
 });
 casper.run();
